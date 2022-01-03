a2f7ea970fdeeb1f45ca37ee33c14e766cb09d65
swkokr <swkokr@gmail.com>
2022-01-04 03:11:04 +0900

    to merge

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from flask_marshmallow import Marshmallow
 
 from datetime import datetime
 
-import json
+import pymysql
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_marshmallow.db'
