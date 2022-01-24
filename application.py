363cd4f42443d18a44431655b01529e14e122f12
swkokr <swkokr@gmail.com>
2022-01-24 19:46:39 +0900

    rand code fix

--- application.py
+++ application.py
@@ -64,8 +64,10 @@ def api():
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
 def check_exists(table, ip):
     return True if table.query.filter(table.ip_address == ip).one() else False
+
 @app.route("/code", methods=['GET', 'POST'])
 def code():
     if request.method == 'POST':
